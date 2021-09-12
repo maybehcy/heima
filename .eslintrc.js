73b7f24b83b0445a90fb90ec1d4bbd69118f1efd
黄昌盈 <2505105031@qq.com>
2021-09-12 15:33:27 +0800

    完成了分类参数开发

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'space-before-function-paren': 0,
-        'vue/no-unused-vars': 'off'
+        'no-unused-vars': 'warn'
 
 
     }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -34,7 +34,10 @@ import {
     Tree,
     Select,
     Option,
-    Cascader
+    Cascader,
+    Alert,
+    Tabs,
+    TabPane
 
 } from 'element-ui'
 
@@ -65,6 +68,10 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
+Vue.use(TabPane)
+Vue.use(Tabs)
+
 
 
 
